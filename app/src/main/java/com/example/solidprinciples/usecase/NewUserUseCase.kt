f90f9540484af3454d39b2af3b3c55dad253df35
Panatchai Vathanasri <vathanasri.panatchai@crypto.com>
2022-11-11 14:09:59 +0700

    Remove confusing code

--- app/src/main/java/com/example/solidprinciples/usecase/NewUserUseCase.kt
+++ app/src/main/java/com/example/solidprinciples/usecase/NewUserUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.solidprinciples.usecase
 
-import com.example.solidprinciples.analytic.SegmentAnalytic
 import com.example.solidprinciples.model.User
 import com.example.solidprinciples.service.EmailService
 import com.example.solidprinciples.service.RegistrationService
@@ -10,20 +9,16 @@ class NewUserUseCase {
     private val registrationService = RegistrationService()
     private val emailService = EmailService()
     private val userService = UserService()
-    private val segmentAnalytic = SegmentAnalytic()
 
     fun registerUser(user: User) {
         registrationService.register(user)
-        segmentAnalytic.track("Register user ${user.name}")
     }
 
     fun sendEmailToUser(user: User) {
         emailService.sendEmailToUser(user)
-        segmentAnalytic.track("Email user ${user.name}")
     }
 
     fun updateUseDetail(user: User) {
         userService.updateUserDetail(user)
-        segmentAnalytic.track("Update user ${user.name}")
     }
 }
